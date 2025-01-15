85115aac1aad8abda73f41894e1fb591e1d56fc8
Rames0 <mhrjan0@gmail.com>
2025-01-15 18:51:31 +0545

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import NepaliCalendar from './Component/Calendar/NepaliCalendar'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <NepaliCalendar/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
